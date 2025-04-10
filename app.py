cc5ef93fbf21c3be02316636e743d2ebd6f42b5b
Yoav Kalisher <ykalisher@gmail.com>
2025-04-10 18:31:35 -0500

    Added Winners for 2020 and 2022

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
-from flask import Flask, redirect, render_template, url_for
-
+from flask import Flask, redirect, render_template, url_for, abort
+from json import load
 app = Flask(__name__, static_url_path='/static')
 
+with open('data.json', 'r') as f:
+	data = load(f)
+	
 @app.get('/')
 def index():
-	# return render_template('Index.html')
-	return redirect(url_for('winners_page', award='rim_protector', year=2025))
+	# TODO: Menu of seasons, winners for this year, best
+	return render_template('Index.html')
 	
-@app.get('/winners/<award>/<year>')
-def winners_page(award, year):	
-	return render_template('winners_page.html', award=award, year=year)
+	
+@app.get('/winners/<award>/<int:year>')
+def winners_page(award, year):
+	if award not in data['clean_award_names'] or str(year) not in data['winners'][award]:
+		abort(404)
+	
+	clean_award_name = data['clean_award_names'][award]
+	winners = data['winners'][award][str(year)]
+	first, second, third = winners['first'], winners['second'], winners['third']
+	description = data['descriptions'][award]
+	return render_template('winners_page.html', award=clean_award_name, year=year, first=first, second=second, third=third, description=description)
+	
+	
+	
+@app.errorhandler(404)
+def not_found(error):
+	#handle the error, for example a custom 404 page.
+	return render_template('404.html'), 404
