0e41c97a01db2fbb9c58564c333e383a7f9f5fdf
Yoav Kalisher <ykalisher@gmail.com>
2025-04-10 16:04:14 -0500

    Base and a couple of placeholder html files

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, redirect, render_template, url_for
 
 app = Flask(__name__, static_url_path='/static')
 
 @app.get('/')
 def index():
-	return render_template('index.html')
+	# return render_template('Index.html')
+	return redirect(url_for('winners_page', award='rim_protector', year=2025))
+	
+@app.get('/winners/<award>/<year>')
+def winners_page(award, year):	
+	return render_template('winners_page.html', award=award, year=year)
